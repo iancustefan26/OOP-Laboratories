229edeaa7b8e0e575cbc2ddf6b53028a986ddbe6
iancustefan26 <stefaniancu46@gmail.com>
2024-03-12 23:23:06 +0200

    lab3 problem 1 Math

---
+++ Lab3/Problema 1 Math/Math.cpp
@@ -0,0 +1,79 @@
+#include "Math.h"
+
+int Math::Add(int a, int b) {
+	return a + b;
+}
+
+int Math::Add(int a, int b, int c) {
+	return a + b + c;
+}
+
+int Math::Add(double a, double b) {
+	return a + b;
+}
+
+int Math::Add(double a, double b, double c) {
+	return a + b + c;
+}
+
+int Math::Mul(int a, int b) {
+	return a * b;
+}
+
+int Math::Mul(int a, int b, int c) {
+	return a * b * c;
+}
+
+int Math::Mul(double a, double b) {
+	return a * b;
+}
+
+int Math::Mul(double a, double b, double c) {
+	return a * b * c;
+}
+
+/*
+int Math::Add(int count, ...) {
+	int result = 0;
+	va_list list;
+	__crt_va_start(list, count);
+
+	for (int i = 0; i < count; ++i)
+		result += __crt_va_arg(list, int);
+
+	__crt_va_end(list);
+
+	return result;
+}
+*/
+
+int Math::Add(int count, ...) {
+	int* p = &count;
+	p += 2;
+	int result = 0;
+	for (int i = 0; i < count; ++i)
+		result += *p, p+=2;
+	return result;
+}
+
+	//SI VARIANTA CU POINTERI
+
+
+char* Math::Add(const char* s, const char* t) {
+	if (s == nullptr || t == nullptr)
+		return nullptr;
+
+	const int lenght = strlen(s) + strlen(t);
+	char* buffer = new char[lenght];
+	char* start = buffer;
+
+	for (int i = 0; i < strlen(s); ++i)
+		*start = *(s + i), start++;
+	for (int i = 0; i < strlen(t); ++i)
+		*start = *(t + i), start++;
+
+	*start = '\0';
+
+	return buffer;
+}
+
---
+++ Lab3/Problema 1 Math/main.cpp
@@ -0,0 +1,19 @@
+#include "Math.h"
+#include <iomanip>
+Math mate;
+
+int main() {
+	
+	std::cout << mate.Add(2, 3) << "\n";
+	std::cout << mate.Add(2, 3, 4) << "\n";
+	std::cout << mate.Add(2.0, 3.6) << "\n";
+	std::cout << mate.Add(2.3, 3.2, 2.1) << "\n";
+	std::cout << mate.Add("Matematica", " este usoara") << "\n";
+	std::cout << mate.Mul(2, 3) << "\n";
+	std::cout << mate.Mul(2, 3, 7) << "\n";
+	std::cout << mate.Mul(2.1, 3.7) << "\n";
+	std::cout << mate.Mul(2.9, 3.1, 10.12) << "\n";
+	std::cout << mate.Add(7, 1, 2, 3, 4, 5, 6, 7);
+
+	return 0;
+}
