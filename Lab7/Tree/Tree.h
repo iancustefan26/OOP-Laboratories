75b186731f0417a0a3e4fd5ecea2b349181edb71
iancustefan26 <stefaniancu46@gmail.com>
2024-04-08 13:02:48 +0300

    finished lab7 Tree template

--- Lab7/Tree/Tree.h
+++ Lab7/Tree/Tree.h
@@ -1,40 +1,55 @@
 #pragma once
 #ifndef TREE_H
 #define TREE_H
+#include <iostream>
+#include <algorithm>
 
 template <class T>
 struct Node {
-	T value;
+	T value = 0;
 	Node<T>* childrens = nullptr;
 	int number_of_childrens = 0;
+
+	Node(T value) {
+		this->value = value;
+	}
+	Node() {}
+	void operator=(Node<T>* a) {
+		*this = a;
+	}
 };
 template <class T>
 class Tree {
 private:
 	Node<T>* root;
+	static bool default_compare(Node<T>& a, Node<T>& b) {
+		return a.value < b.value;
+	}
 public:
 	Tree() {
-		nodes = nullptr;
+		root = new Node<T>();
 	}
-	void add_node(Node<T>* node) {
+	void add_node(Node<T>* node, T value) {
 		if (node == nullptr) {
 			this->root = new Node<T>();
+			this->root->value = value;
 			return;
 		}
-		node->childrens[node->number_of_childrens] = new Node<T>();
+		node->childrens[node->number_of_childrens] = new Node<T>(value);
+		node->number_of_childrens++;
 		node->value = 0;
 	}
-	Node<T>& get_node(Node<T>* node) {
+	Node<T>* get_node(Node<T>* node) {
 		if (node == nullptr) {
-			return this -> *root;
+			return this -> root;
 		}
 		else {
-			return node->*childrens;
+			return node->childrens;
 		}
 	}
-	Node<T>& get_node(Node<T>& node) {
+	Node<T>* get_node(Node<T>& node) {
 		if (&node == nullptr) {
-			return this -> *root;
+			return this -> root;
 		}
 		else {
 			return &node.childrens;
@@ -44,17 +59,33 @@ public:
 		delete[] node->childrens;
 		delete node;
 	}
-	Node<T>* find(T to_find_value, Node<T>* temp, bool* function) {
-		while (temp != nullptr) {
+	Node<T>* find(T to_find_value, Node<T>* temp, bool* function(T, T)) {
+		while (temp != nullptr) {	
 			for (int i = 0; i < temp->number_of_childrens; ++i) {
-				find(to_find_value, temp[i], funtcion);
+				find(to_find_value, temp[i], function);
 				if (function(to_find_value, temp->value))
 					return temp[i];
 			}
 		}
 		return nullptr;
 	}
-
+	void insert(Node<T>* parent, T value) {
+		parent->childrens[parent->number_of_childrens] = new Node<T>(value);
+		parent->number_of_childrens++;
+	}
+	void sort(Node<T>* node, bool* compare(T, T)) {
+		if (compare == nullptr) {
+			std::sort(node->childrens, node->childrens + node->number_of_childrens, default_compare);
+		}
+		std::sort(node->childrens, node->childrens + node->number_of_childrens, compare);
+	}
+	int count(Node<T>* node, const bool initial = false) const {
+		if (node == nullptr && initial == false)
+			return count(this->root);
+		else if (node->childrens != nullptr)
+			return 1 + count(node->childrens + 1, true);
+		else return 0;
+	}
 };
 
 #endif // !TREE_H
